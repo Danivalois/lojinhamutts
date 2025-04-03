7c2487e35bcf60206d3b65532bf0408b06f8f3b5
daniela <valois.daniela@gmail.com>
2025-04-03 18:15:47 -0300

    Final adjustments

--- customers/admin.py
+++ customers/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Customer, Address
+from import_export.admin import ImportExportModelAdmin
 # Register your models here.
 
-admin.site.register(Customer)
-admin.site.register(Address)
+@admin.register(Address)
+class AddressAdmin(ImportExportModelAdmin):
+    list_display = ('customer_zip_code', 'customer_street', 'customer_city')
+    pass
+
+@admin.register(Customer)
+class CustomerAdmin(ImportExportModelAdmin):
+    list_display = ('customer_name', 'customer_cpf')
+    pass
+
--- orders/admin.py
+++ orders/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import Order
 from .models import Freight
+from import_export.admin import ImportExportModelAdmin
 # Register your models here.
 
-admin.site.register(Order)
+@admin.register(Order)
+class OrderAdmin(ImportExportModelAdmin):
+    list_display = ('order_ID', 'order_customer', 'order_product', 'order_status', 'order_created_at')
+    pass
+
+
+#admin.site.register(Order)
 admin.site.register(Freight)
